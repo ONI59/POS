e4151903fa2f3ca3d2196ae1ae00667cde80bfde
Nmhs20 <nurmuhammadhs360@gmail.com>
2024-07-03 01:28:50 +0700

    produk

--- app/Controllers/Produk.php
+++ app/Controllers/Produk.php
@@ -8,7 +8,14 @@ use CodeIgniter\HTTP\ResponseInterface;
 class Produk extends BaseController
 {
     public function index()
-    {
+    {$data = [
+        "judul" => "Master Data",
+        "subjudul" => "Produk",
+        "menu" => "masterdata",
+        "submenu" => "produk",
+        "page" => "v_produk",
+    ];
+    return view("v_template", $data);
         //
     }
 }
