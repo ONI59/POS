9af538edb1d3255d6a0062bce8a6704d722f5fe9
ONI59 <hidayatroni882@gmail.com>
2024-07-03 21:57:50 +0700

    menambah 12 file baru

--- app/Controllers/Kategori.php
+++ app/Controllers/Kategori.php
@@ -3,10 +3,14 @@
 namespace App\Controllers;
 
 use App\Controllers\BaseController;
-use CodeIgniter\HTTP\ResponseInterface;
+use App\Models\ModelKategori;
 
 class Kategori extends BaseController
 {
+    public function __construct()
+    {
+        $this->ModelKategori = new ModelKategori();
+    }
     public function index()
     {
         $data = [
@@ -15,7 +19,36 @@ class Kategori extends BaseController
             "menu" => "masterdata",
             "submenu" => "kategori",
             "page" => "v_kategori",
+            "kategori" =>  $this->ModelKategori->AllData(),
         ];
         return view("v_template", $data);
+    }
+    public function InsertData()
+    {
+        $data = ['nama_kategori' => $this->request->getPost('nama_kategori')];
+        $this->ModelKategori->InsertData($data);
+        session()->setFlashdata('pesan', 'Data Berhasil Ditambahkan !!');
+        return redirect()->to('Kategori');
+    }
+
+    public function UpdateData($id_kategori)
+    {
+        $data = [
+            'id_kategori' => $id_kategori,
+            'nama_kategori' => $this->request->getPost('nama_kategori')
+        ];
+        $this->ModelKategori->UpdateData($data);
+        session()->setFlashdata('pesan', 'Data Berhasil Perbaharui !!');
+        return redirect()->to('Kategori');
+    }
+
+    public function DeleteData($id_kategori)
+    {
+        $data = [
+            'id_kategori' => $id_kategori,
+        ];
+        $this->ModelKategori->DeleteData($data);
+        session()->setFlashdata('pesan', 'Data Berhasil Dihapus !!');
+        return redirect()->to('Kategori');
     }
 }
--- app/Controllers/Satuan.php
+++ app/Controllers/Satuan.php
@@ -3,10 +3,14 @@
 namespace App\Controllers;
 
 use App\Controllers\BaseController;
-use CodeIgniter\HTTP\ResponseInterface;
+use App\Models\ModelSatuan;
 
 class Satuan extends BaseController
 {
+    public function __construct()
+    {
+        $this->ModelSatuan = new ModelSatuan();
+    }
     public function index()
     {
         $data = [
@@ -15,7 +19,37 @@ class Satuan extends BaseController
             "menu" => "masterdata",
             "submenu" => "satuan",
             "page" => "v_satuan",
+            "satuan" => $this->ModelSatuan->AllData(),
         ];
         return view("v_template", $data);
+    }
+
+    public function InsertData()
+    {
+        $data = ['nama_satuan' => $this->request->getPost('nama_satuan')];
+        $this->ModelSatuan->InsertData($data);
+        session()->setFlashdata('pesan', 'Data Berhasil Ditambahkan !!');
+        return redirect()->to('Satuan');
+    }
+
+    public function UpdateData($id_satuan)
+    {
+        $data = [
+            'id_satuan' => $id_satuan,
+            'nama_satuan' => $this->request->getPost('nama_satuan')
+        ];
+        $this->ModelSatuan->UpdateData($data);
+        session()->setFlashdata('pesan', 'Data Berhasil Perbaharui !!');
+        return redirect()->to('Satuan');
+    }
+
+    public function DeleteData($id_satuan)
+    {
+        $data = [
+            'id_satuan' => $id_satuan,
+        ];
+        $this->ModelSatuan->DeleteData($data);
+        session()->setFlashdata('pesan', 'Data Berhasil Dihapus !!');
+        return redirect()->to('Satuan');
     }
 }
