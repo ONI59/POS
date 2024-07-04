b94a8ac77fb3c36e51e18f8037208402bcbab55d
ONI59 <hidayatroni882@gmail.com>
2024-07-04 23:35:34 +0700

    menambahkan file admin dan produk

--- app/Controllers/Admin.php
+++ app/Controllers/Admin.php
@@ -3,30 +3,66 @@
 namespace App\Controllers;
 
 use App\Controllers\BaseController;
+use App\Models\ModelAdmin;
 
 class Admin extends BaseController
 {
+
+    public function __construct()
+    {
+        $this->ModelAdmin = new ModelAdmin();
+    }
+
     public function index()
     {
         $data = [
-            "judul" => "Dashboard",
-            "subjudul" => "",
-            "menu" => "dashboard",
-            "submenu" => "",
-            "page" => "v_admin",
+            'judul' => 'Dashboard',
+            'subjudul' => '',
+            'menu' => 'dashboard',
+            'submenu' => '',
+            'page' => 'v_admin',
+            'grafik' => $this->ModelAdmin->Grafik(),
+            'p_hari_ini' => $this->ModelAdmin->PendapatanHariIni(),
+            'p_bulan_ini' => $this->ModelAdmin->PendapatanBulaniIni(),
+            'p_tahun_ini' => $this->ModelAdmin->PendapatanTahuniIni(),
+            'jml_produk' => $this->ModelAdmin->JumlahProduk(),
+            'jml_kategori' => $this->ModelAdmin->JumlahKategori(),
+            'jml_satuan' => $this->ModelAdmin->JumlahSatuan(),
+            'jml_user' => $this->ModelAdmin->JumlahUser(),
         ];
-        return view("v_template", $data);
+        return view('v_template', $data);
     }
 
     public function Setting()
     {
         $data = [
-            "judul" => "Setting",
-            "subjudul" => "",
-            "menu" => "setting",
-            "submenu" => "",
-            "page" => "v_setting",
+            'judul' => 'Setting',
+            'subjudul' => 'Setting',
+            'menu' => 'setting',
+            'submenu' => '',
+            'page' => 'v_setting',
+            'setting' => $this->ModelAdmin->DetailData(),
         ];
-        return view("v_template", $data);
+        return view('v_template', $data);
+    }
+
+    public function UpdateSetting()
+    {
+        $data = [
+            'id' => '1',
+            'nama_toko' => $this->request->getPost('nama_toko'),
+            'slogan' => $this->request->getPost('slogan'),
+            'alamat' => $this->request->getPost('alamat'),
+            'no_telpon' => $this->request->getPost('no_telpon'),
+            'deskripsi' => $this->request->getPost('deskripsi'),
+        ];
+        $this->ModelAdmin->UpdateData($data);
+        session()->setFlashdata('pesan', 'Data Berhasil Diupdate !!');
+        return redirect()->to('Admin/Setting');
+    }
+
+    public function Cek()
+    {
+        echo dd($this->ModelAdmin->PendapatanTahuniIni());
     }
 }
--- app/Controllers/Produk.php
+++ app/Controllers/Produk.php
@@ -9,10 +9,6 @@ use App\Models\ModelSatuan;
 
 class Produk extends BaseController
 {
-    protected $ModelProduk;
-    protected $ModelKategori;
-    protected $ModelSatuan;
-
     public function __construct()
     {
         $this->ModelProduk = new ModelProduk();
@@ -36,6 +32,94 @@ class Produk extends BaseController
     }
 
     public function InsertData()
-    {   
+    {
+        if ($this->validate([
+            'kode_produk' => [
+                'label' => 'Kode Produk / Barcode',
+                'rules' => 'is_unique[tbl_produk.kode_produk]',
+                'errors' => [
+                    'is_unique' => '{field} Suda Ada, Masukkan Kode Lain !!',
+                ]
+            ],
+            'id_satuan' => [
+                'label' => 'Satuan',
+                'rules' => 'required',
+                'errors' => [
+                    'required' => '{field} Belum Dipilih !!',
+                ]
+            ], 'id_kategori' => [
+                'label' => 'kategori',
+                'rules' => 'required',
+                'errors' => [
+                    'required' => '{field} Belum Dipilih !!',
+                ]
+            ]
+        ])) {
+            $hargabeli = str_replace(",", "", $this->request->getPost('harga_beli'));
+            $hargajual = str_replace(",", "", $this->request->getPost('harga_jual'));
+            $data = [
+                'kode_produk' => $this->request->getPost('kode_produk'),
+                'nama_produk' => $this->request->getPost('nama_produk'),
+                'id_kategori' => $this->request->getPost('id_kategori'),
+                'id_satuan' => $this->request->getPost('id_satuan'),
+                'harga_beli' => $hargabeli,
+                'harga_jual' => $hargajual,
+                'stok' => $this->request->getPost('stok'),
+            ];
+            $this->ModelProduk->InsertData($data);
+            session()->setFlashdata('pesan', 'Data Berhasil Di Tambahkan !!!');
+            return redirect()->to(base_url('Produk'));
+        } else {
+            session()->setFlashdata('errors', \Config\Services::validation()->getErrors());
+            return redirect()->to(base_url('Produk'))->withInput('validation', \Config\Services::validation());
+        }
+    }
+
+    public function UpdateData($id_produk)
+    {
+        if ($this->validate([
+            'id_satuan' => [
+                'label' => 'Satuan',
+                'rules' => 'required',
+                'errors' => [
+                    'required' => '{field} Belum Dipilih !!',
+                ]
+            ], 'id_kategori' => [
+                'label' => 'kategori',
+                'rules' => 'required',
+                'errors' => [
+                    'required' => '{field} Belum Dipilih !!',
+                ]
+            ]
+        ])) {
+            $hargabeli = str_replace(",", "", $this->request->getPost('harga_beli'));
+            $hargajual = str_replace(",", "", $this->request->getPost('harga_jual'));
+            $data = [
+                'id_produk' => $id_produk,
+                'nama_produk' => $this->request->getPost('nama_produk'),
+                'id_kategori' => $this->request->getPost('id_kategori'),
+                'id_satuan' => $this->request->getPost('id_satuan'),
+                'harga_beli' => $hargabeli,
+                'harga_jual' => $hargajual,
+                'stok' => $this->request->getPost('stok'),
+            ];
+            $this->ModelProduk->UpdateData($data);
+            session()->setFlashdata('pesan', 'Data Berhasil Di Update !!!');
+            return redirect()->to(base_url('Produk'));
+        } else {
+            session()->setFlashdata('errors', \Config\Services::validation()->getErrors());
+            return redirect()->to(base_url('Produk'))->withInput('validation', \Config\Services::validation());
+        }
+    }
+
+
+    public function DeleteData($id_produk)
+    {
+        $data = [
+            'id_produk' => $id_produk,
+        ];
+        $this->ModelProduk->DeleteData($data);
+        session()->setFlashdata('pesan', 'Data Berhasil Di Delete !!');
+        return redirect()->to('Produk');
     }
 }
