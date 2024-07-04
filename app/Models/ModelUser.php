b2f10b2ef6cd468750ed7f20bd8b79307d2cfef2
Nmhs20 <nurmuhammadhs360@gmail.com>
2024-07-05 00:00:35 +0700

    menambahkan model user

--- app/Models/ModelUser.php
+++ app/Models/ModelUser.php
@@ -12,20 +12,32 @@ class ModelUser extends Model
             ->get()
             ->getResultArray();
     }
+
     public function InsertData($data)
     {
         $this->db->table('tbl_user')->insert($data);
     }
+
     public function UpdateData($data)
     {
         $this->db->table('tbl_user')
             ->where('id_user', $data['id_user'])
             ->update($data);
     }
+
     public function DeleteData($data)
     {
         $this->db->table('tbl_user')
             ->where('id_user', $data['id_user'])
-            ->Delete($data);
+            ->delete($data);
+    }
+
+    public function LoginUser($email, $password)
+    {
+        return $this->db->table('tbl_user')
+            ->where([
+                'email' => $email,
+                'password' => $password,
+            ])->get()->getRowArray();
     }
 }
